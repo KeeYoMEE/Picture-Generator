3ec6f9d896a5f31a65da04b918d13544415bacef
KeeYoMEE <donkuro@live.com>
2016-04-12 22:45:29 +0200

    Edited

--- index.php
+++ index.php
@@ -2,18 +2,24 @@
 <html>
 <head>
 
-    <title>:)</title>
+    <title>Generator</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
 
 </head>
-<body>
+<body class="seda">
 <div>
-    <nav>
-        <a class="wrapper" href="./main.php">Main</a>
-        <a class="wrapper" href="./login.php">Login</a>
-        <a class="wrapper" href="./register.php">Register</a>
-    </nav>
+<form action="generated.php" method="post">
+    Height: <input type="number" name="height" max="500"><br>
+    Width: <input type="number" name="width" max="500"><br>
+    Choose type of generation:<select name="random">
+        <option value="1">All pixels random</option>
+        <option value="2">Collums all random</option>
+        <option value="3">Collums random but similar</option>
+        <option value="4">Rows all random</option>
+        <option value="5">Rows random but similar</option>
+    <input type="submit">
+</form>
 </div>
 </body>
 </html>
